7dd053e9de32538313fb5a2642ab076fc63a83eb
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2023-10-19 02:25:09 +0530

    Routes added

---
+++ routes/todosRoutes.js
@@ -0,0 +1,44 @@
+import { Router } from "express";
+const router = Router();
+
+router.get("/", async (req, res) => {
+    try {
+        res.json({ todos: [] });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+router.post("/", async (req, res) => {
+    try {
+        res.json({ todos: {} });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+router.get("/:id", async (req, res) => {
+    try {
+        res.json({ todo: {} });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+router.put("/:id", async (req, res) => {
+    try {
+        res.json({ todo: {} });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+        res.json({ todo: {} });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+export default router;
